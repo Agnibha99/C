084e7b3bf8c0a834a6fcc612f167fd6fd6952f41
Agnibha99 <agnibha.18599@gmail.com>
2021-11-23 20:02:02 +0530

    Commit

---
+++ add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+  int a=5,b=6,c;
+  c=a+b;
+  printf("C= %d",c);
+  return 1;
+}
---
+++ add.s
@@ -0,0 +1,36 @@
+	.file	"add.c"
+	.section	.rodata
+.LC0:
+	.string	"C= %d"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB2:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$5, -12(%rbp)
+	movl	$6, -8(%rbp)
+	movl	-12(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	movl	$1, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.12) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
